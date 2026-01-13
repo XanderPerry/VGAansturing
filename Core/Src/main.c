6e3b0e8b0c2d3d290eafe2c8c404d94aa44b6e9c
daniel10642 <daniel10642@live.nl>
2026-01-13 14:33:28 +0100

    Comments toegevoegd om te functioneren met doxygen

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,14 +1,13 @@
-//--------------------------------------------------------------
-// File     : main.c
-// Datum    : 30.03.2016
-// Version  : 1.0
-// Autor    : UB
-// mods by	: J.F. van der Bent
-// CPU      : STM32F4
-// IDE      : CooCox CoIDE 1.7.x
-// Module   : CMSIS_BOOT, M4_CMSIS_CORE
-// Function : VGA_core DMA LIB 320x240, 8bit color
-//--------------------------------------------------------------
+/**
+ * @file main.c
+ * @brief Main code file
+ *
+ * This file contains the main loop that the STM32 will be running
+ *
+ *
+ * @author Tom Veldkamp, Xander Perry & Daniël Wit Ariza
+ * @date 2026-1-13
+ */
 
 #include <API_LIB.h>
 #include "main.h"
